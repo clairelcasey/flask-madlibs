2bd29eb1f08f14d679ee76332ae5e94620ea15fc
Claire Casey <clcasey805@gmail.com>
2020-12-02 16:18:55 -0800

    pre code review

--- app.py
+++ app.py
@@ -7,3 +7,16 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+@app.route('/')
+def show_inputs():
+    """ shows input form from story instance prompts"""
+    return render_template("questions.html", 
+        prompts = silly_story.prompts)
+
+@app.route('/results')
+def show_results():
+    """ generate story from form results and show story on page """
+    story_text = silly_story.generate(request.args)
+    return render_template("story.html", 
+        story = story_text)
--- stories.py
+++ stories.py
@@ -31,7 +31,7 @@ class Story:
 
         for (key, val) in answers.items():
             text = text.replace("{" + key + "}", val)
-
+                        # replace ({verb}, "eat")
         return text
 
 
@@ -42,6 +42,7 @@ silly_story = Story(
     """Once upon a time, in a long-ago {place}, there lived an exceptionally
        {adjective} {noun}. It loved to {verb} with {plural_noun}."""
 )
+
 
 # Here's another --- you should be able to swap in app.py to use this story,
 # and everything should still work
